de502e10d6ca325a731b04acbf039ada6b27d3e7
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-02-26 20:11:20 +0100

    Exercise 91: Completed Reflection Loading point

--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,34 +1,47 @@
 package it.unibo.mvc;
 
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
 import it.unibo.mvc.view.DrawNumberConsoleView;
 import it.unibo.mvc.view.DrawNumberSwingView;
+
+import java.lang.Class;
+import java.lang.reflect.Constructor;
 
 /**
  * Application entry-point.
  */
 public final class LaunchApp {
 
-    private LaunchApp() { }
+    private static final String CONSOLE_VIEW_CLASS = "it.unibo.mvc.view.DrawNumberConsoleView";
+    private static final String SWING_VIEW_CLASS = "it.unibo.mvc.view.DrawNumberSwingView";
+
+    private LaunchApp() {
+    }
 
     /**
      * Runs the application.
      *
      * @param args ignored
-     * @throws ClassNotFoundException if the fetches class does not exist
-     * @throws NoSuchMethodException if the 0-ary constructor do not exist
+     * @throws ClassNotFoundException    if the fetches class does not exist
+     * @throws NoSuchMethodException     if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
-     * @throws InstantiationException if the constructor throws exceptions
-     * @throws IllegalAccessException in case of reflection issues
-     * @throws IllegalArgumentException in case of reflection issues
+     * @throws InstantiationException    if the constructor throws exceptions
+     * @throws IllegalAccessException    in case of reflection issues
+     * @throws IllegalArgumentException  in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws Exception {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberConsoleView());
+        final Class<?> cVC = Class.forName(CONSOLE_VIEW_CLASS);
+        final Constructor<?> cVCConstructor = cVC.getConstructor();
+        final Class<?> sVC = Class.forName(SWING_VIEW_CLASS);
+        final Constructor<?> sVCConstructor = sVC.getConstructor();
+        for (int i = 0; i < 3; i++) {
+            app.addView((DrawNumberView) cVCConstructor.newInstance());
+            app.addView((DrawNumberView) sVCConstructor.newInstance());
+        }
     }
 }
