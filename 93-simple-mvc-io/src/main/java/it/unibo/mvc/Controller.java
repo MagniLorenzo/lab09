4730e3f3a0072251a23fc7f825a6e4063e2552bb
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-03-01 17:45:16 +0100

    Esercizio 93: added javadoc in controller

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -19,18 +19,38 @@ public class Controller {
         this.actualFile = new File(DEFAULT_FILE);
     }
 
+    /**
+     * Set a new destination file
+     * @param file the file where to write
+     */
     public void setFile(final File file) {
         this.actualFile = file;
     }
 
+    /**
+     * Return the current file
+     * 
+     * @return the current file
+     */
     public File getFile() {
         return this.actualFile;
     }
 
+    /**
+     * Return the current file path
+     * 
+     * @return the current file path
+     */
     public String getFilePath() {
         return this.actualFile.getAbsolutePath();
     }
 
+    /**
+     * Save the text on the selected file
+     * 
+     * @param str the text to save
+     * @throws IOException if the print fail
+     */
     public void saveOnFile(final String str) throws IOException {
         try (final PrintStream ps = new PrintStream(actualFile, StandardCharsets.UTF_8)) {
             ps.println(str);
