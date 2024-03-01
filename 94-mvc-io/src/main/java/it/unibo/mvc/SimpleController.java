74e6521b27c425ca06aedadabac2beccce12eaa0
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-03-01 19:37:18 +0100

    Exercise 94: SimpleController implementation completed

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,47 @@
 package it.unibo.mvc;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private Optional<String> actualString;
+    private final List<String> history;
+
+    public SimpleController() {
+        this.actualString = Optional.empty();
+        this.history = new LinkedList<>();
+    }
+
+    @Override
+    public void setString(final String str) {
+        this.actualString = Optional.of(str);
+        this.history.add(str);
+    }
+
+    @Override
+    public String getActualString() {
+        return this.actualString.get();
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return this.history;
+    }
+
+    @Override
+    public void printString() throws IllegalStateException {
+        try {
+            System.out.println(getActualString());
+        } catch (NoSuchElementException e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
 
 }
