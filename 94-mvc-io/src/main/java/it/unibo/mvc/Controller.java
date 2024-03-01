84ee92dee4a3b9931063d9238193752613cdc38b
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-03-01 19:21:33 +0100

    Exercise 94: Controller implementation completed

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,38 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    /**
+     * Set the string to print
+     * 
+     * @param str the string to print
+     */
+    void setString(final String str);
+
+    /**
+     * Return the string that was set to be written
+     * @return the string that was set to be written
+     */
+    String getActualString();
+
+    /**
+     * Return the history of the printed strings
+     * 
+     * @return the history of the printed strings
+     */
+    List<String> getHistory();
+
+    /**
+     * Print the current string
+     * 
+     * @throws IllegalStateException if the current string is unset
+     */
+    void printString() throws IllegalStateException;
+
 
 }
